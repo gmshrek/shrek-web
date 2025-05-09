a3dee69e925e43c3ef36f2bfb4b5421989267fff
gmshrek <gmshrek@163.com>
2025-05-09 09:38:49 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind()],
   site: 'https://your-domain.com', // 替换为您的实际域名
   compressHTML: true,
   vite: {
